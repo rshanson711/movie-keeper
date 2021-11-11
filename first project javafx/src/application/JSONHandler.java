55f3d202aae2e13c947608836c080a9f65779702
Ray Hanson <rshanson711@gmail.com>
2021-11-11 01:12:10 -0600

    added gson support for treading JSONs in the future.

--- first project javafx/src/application/JSONHandler.java
+++ first project javafx/src/application/JSONHandler.java
@@ -1,52 +1,9 @@
 package application;
 import java.net.*;
 import java.io.*;
+import com.google.gson.*;
 
 public class JSONHandler {
-	
-//	public static String executePost(String urlParameters) {
-//		HttpURLConnection connection = null;
-//
-//		try {
-//			//Create connection
-//			URL url = new URL("http://omdbapi.com");
-//			connection = (HttpURLConnection)url.openConnection();
-//			connection.setRequestMethod("POST");
-//			connection.setRequestProperty("Content-Type", 
-//					"application/x-www-form-urlencoded");
-//
-//			connection.setRequestProperty("Content-Length", 
-//					Integer.toString(urlParameters.getBytes().length));
-//			connection.setRequestProperty("Content-Language", "en-US");  
-//
-//			connection.setUseCaches(false);
-//			connection.setDoOutput(true);
-//
-//			//Send request
-//			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-//			wr.writeBytes(urlParameters);
-//			wr.close();
-//
-//			//Get Response  
-//			InputStream is = connection.getInputStream();
-//			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-//			StringBuffer response = new StringBuffer(); // or StringBuffer if Java version 5+
-//			String line;
-//			while ((line = rd.readLine()) != null) {
-//				response.append(line);
-//				response.append('\r');
-//			}
-//			rd.close();
-//			return response.toString();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return null;
-//		} finally {
-//			if (connection != null) {
-//				connection.disconnect();
-//			}
-//		}
-//	}
 	
 	public static String executeGet(String urlP) {
 		try {
@@ -66,10 +23,18 @@ public class JSONHandler {
 			con.disconnect();
 			
 			//new JSONObject(content).toString(2);
-			return content.toString();
+			String jsonData = content.toString();
+			JsonObject jsonEle = JsonParser.parseString(jsonData).getAsJsonObject();
+			
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			String jsonOutput = gson.toJson(jsonEle);
 			
 			
-		}catch(Exception e) {
+			return jsonOutput;
+			//return content.toString();
+			
+			
+		} catch(Exception e) {
 			e.printStackTrace();
 			return null;
 		}
